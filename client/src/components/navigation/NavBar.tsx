13b4e9f15a074f4902ff32a6a86c34d51a5b201d
eleanor-park <eleanor_park@brown.edu>
2024-04-26 20:40:02 -0400

    added some buttons

--- client/src/components/navigation/NavBar.tsx
+++ client/src/components/navigation/NavBar.tsx
@@ -1,9 +1,12 @@
 import { useNavigate } from "react-router-dom";
 import "../../styles/navbar.scss";
 import { useState, useEffect } from "react";
-import { getWeatherData, determineWeatherIcon, mapToImage } from "../pages/HomePage";
-import { PageType } from "../../enums/enums";
-
+import {
+  getWeatherData,
+  determineWeatherIcon,
+  mapToImage,
+} from "../pages/HomePage";
+import { PageType } from "../../items/enums";
 
 export default function NavBar() {
   const navigate = useNavigate();
@@ -28,40 +31,66 @@ export default function NavBar() {
     }
   };
 
-   const [highTemp, setHighTemp] = useState("");
-   const [lowTemp, setLowTemp] = useState("");
-   const [currentTemp, setCurrentTemp] = useState("");
-   const [currentRain, setCurrentRain] = useState(0);
-   const [currentCloud, setCurrentCloud] = useState(0);
-   const [currentSnow, setCurrentSnow] = useState(0);
+  const [highTemp, setHighTemp] = useState("");
+  const [lowTemp, setLowTemp] = useState("");
+  const [currentTemp, setCurrentTemp] = useState("");
+  const [currentRain, setCurrentRain] = useState(0);
+  const [currentCloud, setCurrentCloud] = useState(0);
+  const [currentSnow, setCurrentSnow] = useState(0);
 
-   useEffect(() => {
-     async function fetchWeatherData() {
-       let json = await getWeatherData(43.1536, -93.201);
-       setLowTemp(json.temperature.low);
-       setHighTemp(json.temperature.high);
-       setCurrentTemp(json.temperature.current);
-       setCurrentCloud(json.temperature.cloud);
-       setCurrentRain(json.temperature.rain);
-       setCurrentSnow(json.temperature.snowFall);
-     }
-     fetchWeatherData();
-   }, []);
+  useEffect(() => {
+    async function fetchWeatherData() {
+      let json = await getWeatherData(43.1536, -93.201);
+      setLowTemp(json.temperature.low);
+      setHighTemp(json.temperature.high);
+      setCurrentTemp(json.temperature.current);
+      setCurrentCloud(json.temperature.cloud);
+      setCurrentRain(json.temperature.rain);
+      setCurrentSnow(json.temperature.snowFall);
+    }
+    fetchWeatherData();
+  }, []);
 
   return (
-  <div className="navbar">
+    <div className="navbar">
       <div className="titles-container">
-        <h3 className="pagetitle home" onClick={() => changePage(PageType.Home)}>{"Home"}</h3>
-        <h3 className="pagetitle generate" onClick={() => changePage(PageType.Generate)}>{"Generate"}</h3>
-        <h1 className="maintitle" onClick={() => changePage(PageType.Home)}>{"Fit-Me-UP!"}</h1>
-        <h3 className="pagetitle saved" onClick={() => changePage(PageType.Saved)}>{"Saved"}</h3>
-        <h3 className="pagetitle closet" onClick={() => changePage(PageType.Closet)}>{"Closet"}</h3>
+        <h3
+          className="pagetitle home"
+          onClick={() => changePage(PageType.Home)}
+        >
+          {"Home"}
+        </h3>
+        <h3
+          className="pagetitle generate"
+          onClick={() => changePage(PageType.Generate)}
+        >
+          {"Generate"}
+        </h3>
+        <h1 className="maintitle" onClick={() => changePage(PageType.Home)}>
+          {"Fit-Me-UP!"}
+        </h1>
+        <h3
+          className="pagetitle saved"
+          onClick={() => changePage(PageType.Saved)}
+        >
+          {"Saved"}
+        </h3>
+        <h3
+          className="pagetitle closet"
+          onClick={() => changePage(PageType.Closet)}
+        >
+          {"Closet"}
+        </h3>
       </div>
       <div className="temp-container">
         <h3 className="temptitle temp"> {highTemp}˚</h3>
         <h3 className="currenttitle temp"> {currentTemp}˚ </h3>
         <h3 className="temptitle temp"> {lowTemp}˚</h3>
-        <img src={mapToImage(determineWeatherIcon(currentCloud, currentRain, currentSnow))}/>
+        <img
+          src={mapToImage(
+            determineWeatherIcon(currentCloud, currentRain, currentSnow)
+          )}
+        />
       </div>
     </div>
   );
--- client/src/components/pages/UploadBox.tsx
+++ client/src/components/pages/UploadBox.tsx
@@ -1,41 +1,131 @@
 import { useState, Dispatch, SetStateAction } from "react";
 import "../../styles/uploadbox.scss";
 import { closebutton } from "../../icons/icons";
-import { ClothingType } from "../../enums/enums";
+import { ClothingType, Shape } from "../../items/enums";
+import { ClothingItem } from "../../items/ClothingItem";
 
 export interface UploadBoxProps {
-    setShowAddBox: Dispatch<SetStateAction<boolean>>;
+  setShowAddBox: Dispatch<SetStateAction<boolean>>;
 }
 
-
 export default function UploadBox(props: UploadBoxProps) {
-    const [clothingType, setClothingType] = useState<ClothingType>();
-    const [showSpecificTypes, setShowSpecificTypes] = useState<boolean>(false);
+  const clothingItem = new ClothingItem();
+  const [clothingType, setClothingType] = useState<number>();
+  const [showShapes, setShowShapes] = useState<boolean>(false);
+  const [shapeLabels, setShapeLabels] = useState<[string, Shape][]>([]); // list of tuples for [label, enum]
 
-    function changeClothingType(type: ClothingType) {
-        setClothingType(type);
-        setShowSpecificTypes(true);
-        console.log(clothingType);
+  /**
+   * Handles behavior for when a type button is pressed
+   * @param type enum for clothing type
+   */
+  function handleTypePress(type: ClothingType) {
+    // deselects active type button (and shape button) visually
+    const activeButton = document.getElementsByClassName("type-active");
+    if (activeButton[0]) {
+      activeButton[0].className = "inactive";
+    }
+    const activeShapeButton = document.getElementsByClassName("shape-active");
+    if (activeShapeButton[0]) {
+      activeShapeButton[0].className = "inactive";
     }
     
-    return (
-      <div className="add-box">
-        <img
-          className="close-button"
-          src={closebutton}
-          onClick={() => props.setShowAddBox(false)}
-          aria-label="Close"
-        />
-        <h1> Add to Closet </h1>
-        <h3 className="clothing-type-header"> Clothing Type:</h3>
-        <div className="clothing-types-container" style={{display:"grid"}}>
-          <button onClick={()=>changeClothingType(ClothingType.Top)}>Top</button>
-          <button onClick={()=>changeClothingType(ClothingType.Bottom)}>Bottoms</button>
-          <button onClick={()=>changeClothingType(ClothingType.FullBody)}>Full Body</button>
-          <button onClick={()=>changeClothingType(ClothingType.Shoe)}>Shoe</button>
-          <button onClick={()=>changeClothingType(ClothingType.Outerwear)}>Outerwear</button>
-          <button onClick={()=>changeClothingType(ClothingType.Accessory)}>Accessory</button>
-        </div>
+    if (type === clothingType && showShapes) {
+      setShowShapes(false);
+      clothingItem.type = undefined;
+    } else {
+      setClothingType(type);
+      setShowShapes(true);
+      clothingItem.type = type;
+
+      // selects active button visually
+      const buttonName = "type " + type.toString();
+      const pressedButton = document.getElementById(buttonName);
+      if (pressedButton !== null) {
+        pressedButton.className = "type-active";
+      }
+
+      // create shape labels based on clothing type
+      switch (type) {
+        case ClothingType.Top:
+          setShapeLabels([["Long Sleeve", Shape.LongSleeve], ["Short Sleeve", Shape.ShortSleeve], ["No Sleeve", Shape.NoSleeve]]);
+          break;
+        case ClothingType.Bottom:
+          setShapeLabels([["Pants", Shape.Pants], ["Shorts", Shape.Shorts], ["Skirt", Shape.Skirt]]);
+          break;
+        case ClothingType.FullBody:
+          setShapeLabels([["Dress", Shape.Dress], ["Romper", Shape.Romper], ["Suit", Shape.Suit]]);
+          break;
+        case ClothingType.Shoe:
+          setShapeLabels([["Sneaker", Shape.Sneaker], ["Boot", Shape.Boot], ["Sandal", Shape.Sandal]]);
+          break;
+        case ClothingType.Outerwear:
+          setShapeLabels([["Jacket", Shape.Jacket], ["Sweatshirt", Shape.Sweatshirt], ["Cardigan", Shape.Cardigan]]);
+          break;
+        case ClothingType.Accessory:
+          setShapeLabels([["Headwear", Shape.Headwear], ["Scarf", Shape.Scarf], ["Bag", Shape.Bag]]);
+          break;
+      }
+    }
+  }
+
+  /**
+   * Handles behavior for when a shape button is pressed
+   * @param shape enum for shape
+   */
+  function handleShapeSelection(shape: Shape) {
+    const activeButton = document.getElementsByClassName("shape-active");
+    if (activeButton[0]) {
+      activeButton[0].className = "inactive";
+    }
+    if (clothingItem.shape === shape) {
+        clothingItem.shape === undefined;
+    } else {
+        clothingItem.shape = shape;
+        const buttonName = "shape " + shape.toString();
+        const pressedButton = document.getElementById(buttonName);
+        if (pressedButton !== null) {
+          pressedButton.className = "shape-active";
+        }
+    }
+  }
+
+  function submitItem() {
+    console.log(clothingItem);
+    // if all fields are defined, submit
+    // else "please fill out all fields"
+  }
+
+  return (
+    <div className="add-box">
+      <img
+        className="close-button"
+        src={closebutton}
+        onClick={() => props.setShowAddBox(false)}
+        aria-label="Close"
+      />
+      <h1> Add to Closet </h1>
+      <h3 className="clothing-type-header"> Clothing Type:</h3>
+      <div className="clothing-types-container">
+        <button id="type 0" className="inactive" onClick={() => handleTypePress(ClothingType.Top)}>Top</button>
+        <button id="type 1" className="inactive" onClick={() => handleTypePress(ClothingType.Bottom)}>Bottoms</button>
+        <button id="type 2" className="inactive" onClick={() => handleTypePress(ClothingType.FullBody)}>Full Body</button>
+        <button id="type 3" className="inactive" onClick={() => handleTypePress(ClothingType.Shoe)}>Shoe</button>
+        <button id="type 4" className="inactive" onClick={() => handleTypePress(ClothingType.Outerwear)}>Outerwear</button>
+        <button id="type 5" className="inactive" onClick={() => handleTypePress(ClothingType.Accessory)}>Accessory</button>
       </div>
-    );
+      {showShapes && (
+        <>
+          <h3 className="shapes-header"> Subcategory: </h3>
+          <div className="shapes-container">
+            {shapeLabels.map((label) => (
+              <button id={`shape ${label[1].toString()}`} className="inactive" onClick={()=>handleShapeSelection(label[1])}>{label[0]}</button>
+            ))}
+          </div>
+        </>
+      )}
+      <div className="clothing-types-container">
+
+      </div>
+    </div>
+  );
 }
