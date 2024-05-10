8a48b011cad21fc79c9dc7141b97f4920a59b8bc
eleanor-park <eleanor_park@brown.edu>
2024-05-09 20:13:33 -0400

    fix: avoid querying api too much

--- client/src/components/pages/ClosetPage.tsx
+++ client/src/components/pages/ClosetPage.tsx
@@ -38,7 +38,7 @@ export default function ClosetPage(props: ClosetProps) {
       }));
       props.setClothes(clothesMap);
     });  
-  }, [props.clothes, props.setClothes, setClothes, setClothingFilter, clothes]);
+  }, []);
 
   const getButtonOpacity = (category : string) => {
       return activeButton === category.toString() ? 1 : 0.4; 
