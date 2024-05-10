1b341671a385241dbf313b9e5bb9e7dad3a63499
gdhanda26 <113314719+gdhanda26@users.noreply.github.com>
2024-05-10 01:55:19 -0400

    Update SavedPage.tsx

--- client/src/components/pages/SavedPage.tsx
+++ client/src/components/pages/SavedPage.tsx
@@ -1,38 +1,92 @@
-import { useState, Dispatch, SetStateAction, useEffect, useCallback } from "react";
+import {
+  useState,
+  Dispatch,
+  SetStateAction,
+  useEffect,
+} from "react";
 import "../../styles/savedpage.scss";
 import { listOutfits } from "../../utils/api";
 import { OutfitItem } from "../items/OutfitItem";
 import { showClothing } from "../pages/GeneratePage";
 
-
 export interface SavedPageProps {
-  setClothing: Dispatch<SetStateAction<Map<string, [string, string, string, string]>>>;
+  setClothing: Dispatch<
+    SetStateAction<Map<string, [string, string, string, string]>>
+  >;
   clothing: Map<string, [string, string, string, string]>;
 }
 
 export default function SavedPage(props: SavedPageProps) {
   const [outfitList, setOutfitList] = useState<JSX.Element[]>([]);
-  // const [outfitList, setOutfitList] = useState<Set<JSX.Element>>(
-  //   new Set<JSX.Element>()
-  // );
+  const [hoverIndex, setHoverIndex] = useState<string>("-1");
 
   /**
    * Called to get each saved outfit from the backend and set it into the outfits list.
    */
-  useEffect(() => { 
+  useEffect(() => {
     console.log("using callback");
     listOutfits().then((json) => {
       const outfits: OutfitItem[] = json.clothing;
-      setOutfitList(outfits.map((outfit) => (
-        <div className="outfit-box">
-          {(parseInt(outfit.fullbody) > -1) && <div className="fullbody-box"> {showClothing(outfit.fullbody, props.clothing)} </div> }
-          {(parseInt(outfit.top) > -1) && <div className="top-box"> {showClothing(outfit.top, props.clothing)} </div> }
-          {(parseInt(outfit.bottom) > -1) && <div className="bottom-box"> {showClothing(outfit.bottom, props.clothing)} </div> }
-          {(parseInt(outfit.shoe) > -1) && <div className="shoe-box"> {showClothing(outfit.shoe, props.clothing)} </div> }
-          {(parseInt(outfit.outerwear) > -1) && <div className="outerwear-box"> {showClothing(outfit.outerwear, props.clothing)} </div> }
-          {(parseInt(outfit.accessory) > -1) && <div className="accessory-box"> {showClothing(outfit.accessory, props.clothing)} </div> }
-        </div>
-      )));
+      setOutfitList(
+        outfits.map((outfit) => (
+          <div className="outfit-box">
+            {parseInt(outfit.fullbody) > -1 && (
+              <div
+                className="fullbody-box"
+                onMouseEnter={() => setHoverIndex(outfit.fullbody)}
+                onMouseLeave={() => setHoverIndex("-1")}
+              >
+                {showClothing(outfit.fullbody, props.clothing, hoverIndex)}
+              </div>
+            )}
+            {parseInt(outfit.top) > -1 && (
+              <div
+                className="top-box"
+                onMouseEnter={() => setHoverIndex(outfit.top)}
+                onMouseLeave={() => setHoverIndex("-1")}
+              >
+                {showClothing(outfit.top, props.clothing, hoverIndex)}
+              </div>
+            )}
+            {parseInt(outfit.bottom) > -1 && (
+              <div
+                className="bottom-box"
+                onMouseEnter={() => setHoverIndex(outfit.bottom)}
+                onMouseLeave={() => setHoverIndex("-1")}
+              >
+                {showClothing(outfit.bottom, props.clothing, hoverIndex)}
+              </div>
+            )}
+            {parseInt(outfit.shoe) > -1 && (
+              <div
+                className="shoe-box"
+                onMouseEnter={() => setHoverIndex(outfit.shoe)}
+                onMouseLeave={() => setHoverIndex("-1")}
+              >
+                {showClothing(outfit.shoe, props.clothing, hoverIndex)}
+              </div>
+            )}
+            {parseInt(outfit.outerwear) > -1 && (
+              <div
+                className="outerwear-box"
+                onMouseEnter={() => setHoverIndex(outfit.outerwear)}
+                onMouseLeave={() => setHoverIndex("-1")}
+              >
+                {showClothing(outfit.outerwear, props.clothing, hoverIndex)}
+              </div>
+            )}
+            {parseInt(outfit.accessory) > -1 && (
+              <div
+                className="accessory-box"
+                onMouseEnter={() => setHoverIndex(outfit.accessory)}
+                onMouseLeave={() => setHoverIndex("-1")}
+              >
+                {showClothing(outfit.accessory, props.clothing, hoverIndex)}
+              </div>
+            )}
+          </div>
+        ))
+      );
     });
   }, []);
 
@@ -40,9 +94,7 @@ export default function SavedPage(props: SavedPageProps) {
     <body>
       <h1 className="header">Saved Outfits</h1>
       <div className="saved-page">
-        {outfitList.map((outfit: JSX.Element) => (
-          outfit
-        ))}
+        {outfitList.map((outfit: JSX.Element) => outfit)}
       </div>
     </body>
   );
